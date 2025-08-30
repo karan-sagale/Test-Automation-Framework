389ae03d666a00a90bccdf97dbbb2972e6604b87
Karan <karan.sagale@gmail.com>
2025-08-31 00:20:01 +0530

    Created v1.0 for the Automation Framework and added login test.
    Can execute test on Cloud, Headless, on Different browsers.
    
    Supports parallel execution.

---
+++ src/test/java/com/ui/tests/LoginTest2.java
@@ -0,0 +1,19 @@
+package com.ui.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.ui.pages.HomePage;
+import com.ui.pages.LoginPage;
+
+public class LoginTest2 {
+
+	public static void main(String[] args) {
+		WebDriver wd = new ChromeDriver(); // Launch a Browser Window!! Browser Session is created!!
+	//	HomePage homePage = new HomePage(wd);
+	//	LoginPage loginPage = homePage.goToLoginPage();
+	//	loginPage.doLoginWith("vavic26266@ahanim.com", "password");
+
+	}
+
+}
