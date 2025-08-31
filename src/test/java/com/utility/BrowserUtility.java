85a9af5486a0a0dc2732ce25df7498ea0ba4a12a
Karan <karan.sagale@gmail.com>
2025-09-01 00:16:06 +0530

    Made changes into BrowserUtility class of takeScreenShot method for path into Relative

--- src/test/java/com/utility/BrowserUtility.java
+++ src/test/java/com/utility/BrowserUtility.java
@@ -140,8 +140,8 @@ public abstract class BrowserUtility {
 		Date date = new Date();
 		SimpleDateFormat format = new SimpleDateFormat("HH-mm-ss");
 		String timeStamp = format.format(date);
-	//	String path = System.getProperty("user.dir") + "/screenshots/" + name + " - " + timeStamp + ".png";
-		String path = "./screenshots/" + name + " - " + timeStamp + ".png";
+	//	String path = System.getProperty("user.dir") + "//screenshots//" + name + " - " + timeStamp + ".png";
+	    String path = "./screenshots/" + name + " - " + timeStamp + ".png";
 
 		File screenshotFile = new File(path);
 		try {
